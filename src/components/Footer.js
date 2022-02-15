f5bb2650720948078e0c5b8a4f71a95f1c111742
Diego Santos <diegodeveloper42@gmail.com>
2022-02-15 09:16:04 -0300

    First commit footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,29 @@
 import './style/Footer.css';
+import { FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa';
 export default function Footer() {
   return (
     <footer>
-      <p>Diego Santos</p>
       <ul>
         <li>
           <a href='https://www.instagram.com/calidoweb/?hl=pt-br'
             target='_blank noopener'>
-            
+            <FaInstagram />
           </a>
         </li>
         <li>
           <a href='https://br.linkedin.com/in/drigosantos'
             target='_blank noopener'>
-            
+            <FaLinkedin />
           </a>
         </li>
         <li>
           <a href='https://github.com/odiegosantos'
             target='_blank nooperner'>
-           
+            <FaGithub />
           </a>
         </li>
       </ul>
+      <p className='name'>Diego Santos</p>
     </footer>
   )
 };
